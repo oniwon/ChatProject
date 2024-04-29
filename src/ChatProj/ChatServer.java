9186b262a9b48201fc5150ccf3cc0cb15a56d883
oniwon <cookie89800@naver.com>
2024-04-29 14:23:22 +0900

    server 수정

--- src/ChatProj/ChatServer.java
+++ src/ChatProj/ChatServer.java
@@ -8,9 +8,6 @@ public class ChatServer {
     public static void main(String[] args) {
         try(ServerSocket serverSocket = new ServerSocket(12345);
         ) {
-            //List // 모든 연결 클라이언트 소켓 목록
-            //List<Socket> socketlist = new ArrayList<>();
-            //Map<String, List<Member>> rooms // id 와 방 멤버 정보(socket, nickname)
             Map<Integer, List<Member>> rooms = new HashMap<>();
 
             while (true) {
@@ -18,8 +15,7 @@ public class ChatServer {
 
                 //클라이언트가 접속하면 서버는 사용자의 IP 주소를 출력합니다.
                 System.out.println("Client IP: " + socket.getInetAddress());
-
-                //socketlist.add(socket);
+                
 
                 //쓰레드 실행
                 new ServerThread(socket, rooms).start();
